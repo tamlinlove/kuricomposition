b4a29b337586d6166f1943e7c11612a3dd3be6da
tamlinlove <tamlinlollislove@gmail.com>
2022-03-18 09:55:41 +0200

    Tweaked network script

--- network stuff/client_script.py
+++ network stuff/client_script.py
@@ -6,13 +6,29 @@ servers = ["localhost"]
 ports = [12007]
 sockets = [0]
 
+FORWARD = [0.6,0,0,0,0,0]
+TURN_LEFT = [0,0,0,0,0,1.6]
+TURN_RIGHT = [0,0,0,0,0,-1.6]
+
 
 def run():
-    for i in range(10):
-        limitSet = [i,0,0,0,0,0]
+    while True:
+        command = input()
+        if command == "w":
+            limitSet = FORWARD
+        elif command == "a":
+            limitSet = TURN_LEFT
+        elif command == "d":
+            limitSet = TURN_RIGHT
+        elif command == "q":
+            break
+        else:
+            print("Command not recognised. Must be w, a, d or q")
+            continue
+
         client.send_to_server(limitSet, servers[0], sockets[0]) #Sending sets to servers
         results = client.get_replies(sockets[0])
-        time.sleep(2)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
